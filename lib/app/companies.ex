63ab0d0f52ec6955cebbd785f8d29c9481ae3ad9
Luis Felipe Rodrigues <felipe@Felipe>
2024-07-27 21:52:54 -0300

    feat: repo and anonimazation

---
+++ lib/app/companies.ex
@@ -0,0 +1,6 @@
+defmodule App.Companies do
+  import Ecto.Query, warn: false
+  require Logger
+  alias App.Repo
+  alias App.Companies.Company
+end
---
+++ lib/app/customers.ex
@@ -0,0 +1,41 @@
+defmodule App.Customers do
+  import Ecto.Query, warn: false
+  require Logger
+  alias App.Repo
+  alias App.Customers.Customer
+
+  def find_all_customers(company_id) do
+    from(c in Customer)
+    |> where([c], c.company_id == ^company_id)
+    |> where([c], is_nil(c.deleted_at))
+    |> Repo.all()
+    |> case do
+      [] -> {:error, "No customers found"}
+      customers -> {:ok, customers}
+    end
+  end
+
+  def find_customer(cpf, company_id) do
+    from(c in Customer)
+    |> where([c], c.cpf == ^cpf and c.company_id == ^company_id)
+    |> where([c], is_nil(c.deleted_at))
+    |> Repo.one()
+    |> case do
+      nil -> {:error, "Customer not found"}
+      customer -> {:ok, customer}
+    end
+  end
+
+  def delete_customer(cpf, company_id) do
+    customer = find_customer(cpf, company_id)
+
+    case customer do
+      {:ok, customer} ->
+        Customer.delete_changeset(customer) |> Repo.update()
+        Logger.info("Customer deleted: #{inspect(customer.full_name)}")
+
+      {:error, e} ->
+        Logger.error("Error deleting customer: #{inspect(e)}")
+    end
+  end
+end
