eb0817d3f5604e6768f8542298286250bfe571f7
Luis Felipe Rodrigues <felipe@Felipe>
2024-08-03 15:33:37 -0300

    schema: refactoring schemas

--- lib/app/sales.ex
+++ lib/app/sales.ex
@@ -4,37 +4,7 @@ defmodule App.Sales do
   alias App.Repo
   alias App.Sales.Sale
 
-  def sales_by_customer_id(""), do: []
-
-  def sales_by_customer_id(customer_id) do
-    from(s in Sale)
-    |> join(:left, [s], c in assoc(s, :customer))
-    |> where([s, c], s.customer_id == ^customer_id)
-    |> select(
-      [s, c],
-      %{
-        value: s.total_value,
-        installment: s.installment,
-        which_payment: s.which_payment,
-        which_process: s.which_process,
-        is_paid: s.is_paid,
-        id: s.id,
-        created_at: s.created_at,
-        customer_name: c.full_name,
-        customer_phone: c.mobile_phone,
-        customer_email: c.email,
-        customer_image: c.image_url,
-        customer_cpf: c.cpf,
-        customer_id: c.id
-      }
-    )
-    |> order_by([s], desc: s.created_at)
-    |> Repo.all()
-  end
-
-  def sales_by_company_id(""), do: []
-
-  def sales_by_company_id(options, company_id) when is_map(options) do
+  def find_sales_by_customer_id(customer_id, company_id) do
     from(s in Sale)
     |> join(:left, [s], c in assoc(s, :customer))
     |> select(
@@ -42,34 +12,101 @@ defmodule App.Sales do
       %{
         id: s.id,
         customer_name: c.full_name,
+        customer_mobile_phone: c.mobile_phone,
         which_process: s.which_process,
         total_value: s.total_value,
         which_payment: s.which_payment,
         is_paid: s.is_paid,
-        created_at: s.created_at
+        created_at: s.created_at,
+        installment: s.installment
       }
     )
-    |> where([s, c], c.company_id == ^company_id)
-    |> sort(options)
-    |> paginate(options)
+    |> where([s, _], s.customer_id == ^customer_id)
+    |> where([_, c], c.company_id == ^company_id)
+    |> order_by([s], desc: s.created_at)
     |> Repo.all()
   end
 
-  def most_payment_method(company_id) do
+  def latest_sales(company_id) do
+    from(s in Sale)
+    |> join(:left, [s], c in assoc(s, :customer))
+    |> select(
+      [s, c],
+      %{
+        id: s.id,
+        customer_name: c.full_name,
+        customer_mobile_phone: c.mobile_phone,
+        which_process: s.which_process,
+        total_value: s.total_value,
+        which_payment: s.which_payment,
+        is_paid: s.is_paid,
+        created_at: s.created_at,
+        installment: s.installment
+      }
+    )
+    |> where([s, c], c.company_id == ^company_id)
+    |> order_by([s], desc: s.created_at)
+    |> limit(5)
+    |> Repo.all()
+  end
+
+  def most_payment_method_year(company_id) do
+    start_of_year = Timex.beginning_of_year(NaiveDateTime.utc_now())
+    end_of_year = Timex.end_of_year(NaiveDateTime.utc_now())
+
     from(s in Sale)
     |> select([s], %{count_payment: count(s.which_payment), which_payment: s.which_payment})
     |> join(:left, [s], c in assoc(s, :customer))
     |> where([s, c], c.company_id == ^company_id and s.exemption == false)
+    |> where([s], s.exemption == false)
+    |> where([c], c.created_at >= ^start_of_year and c.created_at <= ^end_of_year)
     |> group_by([s], s.which_payment)
     |> order_by([s], desc: count(s.which_payment))
     |> limit(1)
     |> Repo.one()
   end
 
-  def most_sold_process(company_id) do
+  def most_payment_method_month(company_id) do
+    start_of_month = Timex.beginning_of_month(NaiveDateTime.utc_now())
+    end_of_month = Timex.end_of_month(NaiveDateTime.utc_now())
+
+    from(s in Sale)
+    |> select([s], %{count_payment: count(s.which_payment), which_payment: s.which_payment})
+    |> join(:left, [s], c in assoc(s, :customer))
+    |> where([s, c], c.company_id == ^company_id and s.exemption == false)
+    |> where([s], s.exemption == false)
+    |> where([c], c.created_at >= ^start_of_month and c.created_at <= ^end_of_month)
+    |> group_by([s], s.which_payment)
+    |> order_by([s], desc: count(s.which_payment))
+    |> limit(1)
+    |> Repo.one()
+  end
+
+  def most_sold_process_year(company_id) do
+    start_of_year = Timex.beginning_of_year(NaiveDateTime.utc_now())
+    end_of_year = Timex.end_of_year(NaiveDateTime.utc_now())
+
     from(s in Sale)
     |> join(:left, [s], c in assoc(s, :customer))
     |> where([s, c], c.company_id == ^company_id and s.exemption == false)
+    |> where([s], s.exemption == false)
+    |> where([c], c.created_at >= ^start_of_year and c.created_at <= ^end_of_year)
+    |> group_by([s], s.which_process)
+    |> order_by([s], desc: count(s.which_process))
+    |> select([s], %{count_process: count(s.which_process), which_process: s.which_process})
+    |> limit(1)
+    |> Repo.one()
+  end
+
+  def most_sold_process_month(company_id) do
+    start_of_month = Timex.beginning_of_month(NaiveDateTime.utc_now())
+    end_of_month = Timex.end_of_month(NaiveDateTime.utc_now())
+
+    from(s in Sale)
+    |> join(:left, [s], c in assoc(s, :customer))
+    |> where([s, c], c.company_id == ^company_id and s.exemption == false)
+    |> where([s], s.exemption == false)
+    |> where([c], c.created_at >= ^start_of_month and c.created_at <= ^end_of_month)
     |> group_by([s], s.which_process)
     |> order_by([s], desc: count(s.which_process))
     |> select([s], %{count_process: count(s.which_process), which_process: s.which_process})
@@ -78,26 +115,26 @@ defmodule App.Sales do
   end
 
   def sales_year(company_id) do
+    start_of_year = Timex.beginning_of_year(NaiveDateTime.utc_now())
+    end_of_year = Timex.end_of_year(NaiveDateTime.utc_now())
+
     from(s in Sale)
     |> join(:left, [s], c in assoc(s, :customer))
-    |> where(
-      [s, c],
-      c.company_id == ^company_id and s.exemption == false
-    )
+    |> where([s, c], c.company_id == ^company_id)
+    |> where([s], s.exemption == false)
+    |> where([s], s.created_at >= ^start_of_year and s.created_at <= ^end_of_year)
     |> Repo.aggregate(:sum, :total_value)
   end
 
-  def sales_month(company_id, days \\ 31) do
+  def sales_month(company_id) do
+    start_of_month = Timex.beginning_of_month(NaiveDateTime.utc_now())
+    end_of_month = Timex.end_of_month(NaiveDateTime.utc_now())
+
     from(s in Sale)
     |> join(:left, [s], c in assoc(s, :customer))
-    |> where(
-      [s, c],
-      c.company_id == ^company_id and s.exemption == false
-    )
-    |> where(
-      [c, s],
-      fragment("? - ? <= ? * interval '1 day'", ^NaiveDateTime.utc_now(), c.created_at, ^days)
-    )
+    |> where([s], s.exemption == false)
+    |> where([s, c], c.company_id == ^company_id and s.exemption == false)
+    |> where([c], c.created_at >= ^start_of_month and c.created_at <= ^end_of_month)
     |> Repo.aggregate(:sum, :total_value)
   end
 
