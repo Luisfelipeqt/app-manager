95fb945be468fde061298d83f73eec4694f7a2f2
Luis Felipe Rodrigues <felipe@Felipe>
2024-08-03 15:35:23 -0300

    front: creating and refactoring the customers page

--- lib/app_web/live/customers_live/index.ex
+++ lib/app_web/live/customers_live/index.ex
@@ -5,11 +5,13 @@ defmodule AppWeb.CustomersLive.Index do
   alias App.Sales
 
   def mount(%{"id" => id}, _session, socket) do
-    {:ok, customer} = Customers.find_customer(id, socket.assigns.current_user.company_id)
-    sales = Sales.sales_by_customer_id(id)
+    company_id = socket.assigns.current_user.company_id
+    customer = Customers.find_customer(id, company_id)
+    sales = Sales.find_sales_by_customer_id(id, company_id)
     dbg(id)
 
     socket
+    |> assign(:page_title, "Detalhes do Cliente")
     |> assign(:customer, customer)
     |> stream(:sales, sales)
     |> dbg()
