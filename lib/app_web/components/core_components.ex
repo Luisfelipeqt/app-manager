f726b2fbd5ea903c3201ce88087f8e249fef326f
Luis Felipe Rodrigues <felipe@Felipe>
2024-08-05 00:07:34 -0300

    front: refactoring core_components.ex table

--- lib/app_web/components/core_components.ex
+++ lib/app_web/components/core_components.ex
@@ -500,15 +500,11 @@ defmodule AppWeb.CoreComponents do
           phx-update={match?(%Phoenix.LiveView.LiveStream{}, @rows) && "stream"}
           class="relative divide-y divide-zinc-100 border-t border-zinc-200 text-sm leading-6"
         >
-          <tr
-            :for={row <- @rows}
-            id={@row_id && @row_id.(row)}
-            class="hover:bg-green-600/75 dark:hover:bg-rose-600/75"
-          >
+          <tr :for={row <- @rows} id={@row_id && @row_id.(row)} class="">
             <td
               :for={{col, i} <- Enum.with_index(@col)}
               phx-click={@row_click && @row_click.(row)}
-              class={["relative p-0", @row_click && "hover:cursor-pointer"]}
+              class={["relative p-0", @row_click && ""]}
             >
               <div class="block py-4 pr-6">
                 <span class="absolute -inset-y-px right-0 -left-4 group-hover:bg-zinc-50 sm:rounded-l-xl" />
