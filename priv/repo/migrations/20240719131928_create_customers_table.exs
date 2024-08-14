9ee4c4b833f815c12d2c0f5f7b639457cc951f66
Luis Felipe Rodrigues <felipe@Felipe>
2024-08-13 23:15:22 -0300

    chore: refactoring the migrations, mobile_phone and cpf size

--- priv/repo/migrations/20240719131928_create_customers_table.exs
+++ priv/repo/migrations/20240719131928_create_customers_table.exs
@@ -5,10 +5,10 @@ defmodule App.Repo.Migrations.CreateCustomersTable do
     create table(:customers) do
       add :gender, :string
       add :birthday, :date
-      add :cpf, :char, size: 14, null: false
+      add :cpf, :string, size: 14, null: false
       add :email, :string, null: false
       add :full_name, :string, null: false
-      add :mobile_phone, :char, size: 20, null: false
+      add :mobile_phone, :string, size: 20, null: false
       add :registration, :date
       add :image_url, :string, null: false, default: "/images/default-user.svg"
 
@@ -17,14 +17,15 @@ defmodule App.Repo.Migrations.CreateCustomersTable do
       add :locality, :string, null: false, default: "Não informado"
       add :neighborhood, :string, null: false, default: "Não informado"
       add :state, :char, size: 2, null: false, default: "SP"
-      add :zip_code, :string, size: 10, null: false
+
+      add :zip_code, :string, size: 9, null: false
 
       add(:is_active, :boolean, default: true, null: false)
       add(:is_blocked, :boolean, default: false, null: false)
 
       add(
         :company_id,
-        references(:companies, type: :binary_id, on_delete: :delete_all, on_update: :update_all),
+        references(:companies, type: :binary_id, on_delete: :nothing, on_update: :update_all),
         null: false
       )
 
@@ -33,9 +34,6 @@ defmodule App.Repo.Migrations.CreateCustomersTable do
     end
 
     create index(:customers, [:cpf, :company_id])
-    create unique_index(:customers, :cpf)
-    create unique_index(:customers, :email)
-    create unique_index(:customers, :mobile_phone)
 
     execute "CREATE EXTENSION IF NOT EXISTS pg_trgm;"
 
@@ -43,6 +41,24 @@ defmodule App.Repo.Migrations.CreateCustomersTable do
       CREATE INDEX customers_full_name_gin_trgm_idx
         ON customers
         USING gin (full_name gin_trgm_ops);
+    """
+
+    execute """
+      CREATE INDEX customers_cpf_gin_trgm_idx
+        ON customers
+        USING gin (cpf gin_trgm_ops);
+    """
+
+    execute """
+      CREATE INDEX customers_email_gin_trgm_idx
+        ON customers
+        USING gin (email gin_trgm_ops);
+    """
+
+    execute """
+    CREATE INDEX customers_mobile_phone_gin_trgm_idx
+      ON customers
+      USING gin (mobile_phone gin_trgm_ops);
     """
   end
 
